0821cd28e8cdde77c0460a538488aa8efd47a9a4
Roli15 <81290196+Roli15@users.noreply.github.com>
2024-05-03 09:44:45 +0200

    Update traj_constraints_cc.m
    fixed task 10

--- templates/traj_constraints_cc.m
+++ templates/traj_constraints_cc.m
@@ -12,14 +12,14 @@ function [T1_max, T2_min, T2_max, P1_min, P1_max, P2_min, P2_max, input_cost, cs
     T2_min = min(X(2,:));
     T2_max = max(X(2,:));
     P1_min = min(U(1,:));
-    P2_max = max(U(2,:));
-    P2_min = min(U(2,:));
     P1_max = max(U(1,:));
+    P2_min = min(U(2,:));
+    P2_max = max(U(2,:));
 
     u_max = [P1_max; P2_max];
     u_min = [P1_min; P2_min];
-    x_max = [T1_max; T2_max; 0];
-    x_min = [0 ; T2_min; 0];
+    x_max = [T1_max; T2_max; max(X(1,:))];
+    x_min = [min(X(1,:)) ; T2_min; min(X(3,:))];
     
     input_cost = 0;
     for Ut = U
@@ -27,7 +27,10 @@ function [T1_max, T2_min, T2_max, P1_min, P1_max, P2_min, P2_max, input_cost, cs
     end
 
     [H_u, h_u, H_x, h_x] = generate_constraints_cc(params);
-    cstr_viol = (H_u*u_max > h_u) & (H_u*u_min > h_u);
-    cstr_viol = cstr_viol & (H_x*x_max > h_x) & (H_x*x_min > h_x);
+    cstr_viol_u = (H_u*u_max > h_u) | (H_u*u_min > h_u);
+    cstr_viol_u = sum(cstr_viol_u) > 0;
+    cstr_viol_x = (H_x*x_max > h_x) | (H_x*x_min > h_x);
+    cstr_viol_x = sum(cstr_viol_x) > 0;
+    cstr_viol = (cstr_viol_u | cstr_viol_x);
 end
 
