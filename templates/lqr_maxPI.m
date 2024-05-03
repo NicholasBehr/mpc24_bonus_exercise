226b903c15f7e008c3f5bf7ae18937f1b0632676
Roli15 <81290196+Roli15@users.noreply.github.com>
2024-05-03 13:52:18 +0200

    task 12 done and tested

--- templates/lqr_maxPI.m
+++ templates/lqr_maxPI.m
@@ -8,5 +8,38 @@
 
 function [H, h] = lqr_maxPI(Q, R, params)
 
+obj = LQR(Q,R,params);
+K = obj.K;
+ 
+A = params.model.A;
+B = params.model.B;
+ 
+Hx = params.constraints.StateMatrix;
+hx = params.constraints.StateRHS;
+Hu = params.constraints.InputMatrix;
+hu = params.constraints.InputRHS;
+ 
+H_aug = [Hx;Hu*K];
+h_aug = [hx; hu];
+ 
+system = LTISystem('A', A+B*K);
+Px = Polyhedron('H', [H_aug h_aug]);
+
+% Px.isBounded()
+% Px.isFullDim()
+% Px.plot()
+ 
+system.x.with('setConstraint');
+system.x.setConstraint = Px;
+InvSet = system.invariantSet();
+
+H = InvSet.A;
+h = InvSet.b;
+ 
+% P = Polyhedron('H', [H h]);
+% P.isBounded()
+% P.isEmptySet()
+% P.plot()
+
 end
 
