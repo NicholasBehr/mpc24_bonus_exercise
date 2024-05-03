7f2b7c40b075384c5035a8a2c6f0adf0d0e21f46
Robin Feuz <robin.feuz@e-sling.com>
2024-05-03 10:15:53 +0200

    Task11, not finished yet

--- templates/lqr_tuning_script_cc.m
+++ templates/lqr_tuning_script_cc.m
@@ -8,6 +8,20 @@
 
 % CONTINUE BELOW THIS LINE
 
+params = generate_params_cc();
+params_delta = generate_params_delta_cc(params);
+
+q = [10, 100 , 0];
+Q = diag(q);
+r = [1.2e-5 , 1.8e-5];
+R = diag(r);
+
+Controller = LQR(Q, R, params_delta);
+[X, U, ctrl_info] = simulate(params_delta.exercise.InitialConditionA, Controller, params_delta);
+
+% [X_abs, U_abs] = traj_delta2abs(X, U, params_delta.exercise.x_s, params_delta.exercise.u_s);
+plot_trajectory_cc(X, U, ctrl_info, params_delta);
+
 %% Save results
 current_folder = fileparts(which(mfilename));
 save(fullfile(current_folder, "lqr_tuning_script_cc.mat"), 'Q', 'R', 'X', 'U');
