a268f82f4268412d6fe00f334fdf50328ac2cc22
Nicholas Behr <nbehr@student-net-cx-1096.intern.ethz.ch>
2024-05-10 10:33:02 +0200

    task 24 done

--- templates/generate_params_aug_obs.m
+++ templates/generate_params_aug_obs.m
@@ -8,4 +8,17 @@
 
 function [params_aug_obs] = generate_params_aug_obs(params)
 
+params_aug_obs = params;
+
+params_aug_obs.model.A = [params.model.A , params.model.Bd; 
+    zeros(params.model.nd, params.model.nx), eye(params.model.nd)];
+
+params_aug_obs.model.B = [params.model.B; 
+    zeros(params.model.nd, params.model.nu)];
+
+params_aug_obs.model.C = [params.model.C, params.model.Cd];
+
+params_aug_obs.model.nx = params.model.nx + params.model.nd;
+
+params_aug_obs.constraints.StateMatrix = [params.constraints.StateMatrix, zeros(size(params.constraints.StateMatrix,1), params.model.nd)];
 end
