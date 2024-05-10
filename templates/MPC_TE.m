4797eb940601f97aa3fadd2997ee75bde0509eb1
Nicholas Behr <nbehr@student-net-cx-1096.intern.ethz.ch>
2024-05-10 09:49:51 +0200

    task 16

--- templates/MPC_TE.m
+++ templates/MPC_TE.m
@@ -13,11 +13,26 @@ classdef MPC_TE
 
     methods
         function obj = MPC_TE(Q,R,N,params)
-
-            % ADD STUFF HERE
+            u = sdpvar(repmat(params.model.nu,1,N),repmat(1,1,N));
+            x = sdpvar(repmat(params.model.nx,1,N+1),repmat(1,1,N+1));
+            
+            constraints = [];
+            objective = 0;
+            
+            Hx = params.constraints.StateMatrix;
+            hx = params.constraints.StateRHS;
+            Hu = params.constraints.InputMatrix;
+            hu = params.constraints.InputRHS;
+                       
+            for k = 1:N
+                objective = objective + x{k}'*Q*x{k} + u{k}'*R*u{k};
+                constraints = [constraints, x{k+1} == params.model.A*x{k} + params.model.B*u{k}];
+                constraints = [constraints, Hx * x{k} <= hx, Hu * u{k} <= hu];
+            end
+            constraints = [constraints, norm(x{N+1}, 1) == 0];
 
             opts = sdpsettings('verbose',1,'solver','quadprog');
-            obj.yalmip_optimizer = optimizer(constraints,objective,opts,X0,{U{1} objective});
+            obj.yalmip_optimizer = optimizer(constraints,objective,opts,x{1},{u{1} objective});
         end
 
         function [u, ctrl_info] = eval(obj,x)
