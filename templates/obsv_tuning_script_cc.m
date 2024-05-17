d667ef7e062dd68a19f410fb04c9559ebda15d49
NicholasBehr <github.fc3r26vy@mailalias.me>
2024-05-17 16:17:59 +0200

    task 28 - observer tuning completed

--- templates/obsv_tuning_script_cc.m
+++ templates/obsv_tuning_script_cc.m
@@ -10,7 +10,6 @@
 params = generate_params_cc();
 params_aug_obs = generate_params_aug_obs(params);
 
-poles = [0.1, 0.2, 0.3, 0.3, 0.2, 0.1];
 x0 = params.exercise.InitialConditionA;
 radiation = params.exercise.RadiationB;
 d = [params.model.a1o; params.model.a2o; params.model.a3o] * ...
@@ -20,7 +19,17 @@ Disturbances = ones(params.model.nd,params.exercise.SimHorizon+1).*d;
 x0_est = params.exercise.x0_est;
 d0_est = params.exercise.d0_est;
 
-L = compute_observer_gain(poles, params_aug_obs);
+% cost matrices
+Q = eye(params_aug_obs.model.nx) * 15;
+R = eye(params_aug_obs.model.ny);
+
+% compute feedback matrix and polesx
+A_obs = params_aug_obs.model.A;
+C_obs = params_aug_obs.model.C;
+
+[~,K,poles] = idare(A_obs',C_obs',Q,R);
+L = -K';
+poles = poles';
 
 obsv = Linear_Observer(L, params_aug_obs);
 ctrl = Controller_constant();
